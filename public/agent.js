e3a28e808f6ccb6e2e35c3fca2384b179bbbc009
Dan1599 <dan1599@my.yorku.ca>
2022-03-24 17:02:24 -0400

    Update agent.js

--- public/agent.js
+++ public/agent.js
@@ -74,8 +74,8 @@ class Agent {
     //make the sphere move towards a specific point
     this.sphere.attractionPoint(this.speed/25, this.pos.x, this.pos.y);
 
-      //camera.position.x = this.sphere.position.x;
-      //camera.position.y = this.sphere.position.y;
+     // camera.position.x = this.sphere.position.x;
+     // camera.position.y = this.sphere.position.y;
   }
 
   /**
